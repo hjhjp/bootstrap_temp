20dfb41d1ccc1c45bc9f6aa592dd5a4fbe21ff64
hjhjp <hjhjp0206@gmail.com>
2022-02-10 15:05:54 +0900

    js 수정

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', event => {  
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    let navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
